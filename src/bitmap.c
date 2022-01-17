48a0a7a357d1843fc5fcf3d2bd8de899d4bca18d
jani-nykanen <jnyknn@gmail.com>
2022-01-17 22:09:26 +0200

    Sprite sheet creation; not tested yet

--- src/bitmap.c
+++ src/bitmap.c
@@ -16,6 +16,17 @@ typedef struct {
 } _Bitmap;
 
 
+typedef struct {
+
+    u16 width;
+    u16 height;
+    u16 count;
+
+    _Bitmap** sprites;
+
+} _SpriteSheet;
+
+
 static bool next_char(u8* out, FILE* f) {
 
     i16 c = fgetc(f);
@@ -50,7 +61,7 @@ static void decode_RLE(u8* data, u16 size, FILE* f) {
 }
 
 
-Bitmap* new_bitmap(u16 width, u16 height, bool createMash) {
+Bitmap* new_bitmap(u16 width, u16 height, bool createMask) {
 
     _Bitmap* bmp = (_Bitmap*) calloc(1, sizeof(_Bitmap));
     if (bmp == NULL) {
@@ -71,7 +82,7 @@ Bitmap* new_bitmap(u16 width, u16 height, bool createMash) {
     }
 
     bmp->mask = NULL;
-    if (createMash) {
+    if (createMask) {
 
         bmp->mask = (u8*) calloc(width*height, sizeof(u8));
         if (bmp->mask == NULL) {
@@ -83,6 +94,46 @@ Bitmap* new_bitmap(u16 width, u16 height, bool createMash) {
     }
 
     return (Bitmap*) bmp;
+}
+
+
+Bitmap* create_bitmap_from_data(u16 width, u16 height, 
+    u8* pixels, u8* mask, u32 start,  u32 offset) {
+
+    u16 x, y;
+    u32 i;
+
+    _Bitmap* bmp = (_Bitmap*) new_bitmap(width, height, mask != NULL);
+    if (bmp == NULL) {
+
+        return NULL;
+    }
+
+    i = start;
+    for (y = 0; y < height; ++ y) {
+
+        for (x = 0; x < width; ++ x, ++ i) {
+
+            bmp->pixels[y*width + x] = pixels[i];
+        }
+        i += offset - width;
+    }
+
+
+    if (mask != NULL) {
+
+        i = start;
+        for (y = 0; y < height; ++ y) {
+
+            for (x = 0; x < width; ++ x, ++ i) {
+
+                bmp->mask[y*width + x] = mask[i];
+            }
+            i += offset - width;
+        }
+    }
+
+    return (Bitmap*) bmp; 
 }
 
 
@@ -148,3 +199,70 @@ void dispose_bitmap(Bitmap* _bmp) {
     m_free(bmp);
 }
 
+
+SpriteSheet* create_sprite_sheet_from_bitmap(Bitmap* _bmp, u16 width, u16 height) {
+    
+    _SpriteSheet* sheet;
+    _Bitmap* bmp = (_Bitmap*) _bmp;
+    i16 x, y;
+    u16 w, h;
+    u32 start;
+
+    if (bmp->width % width != 0 || bmp->height % height != 0) {
+
+        m_throw_error("Could not create a sprite sheet from a bitmap: invalid dimensions.", NULL, NULL);
+        return NULL;
+    }
+    
+    sheet = (_SpriteSheet*) calloc(1, sizeof(_SpriteSheet));
+    if (sheet == NULL) {
+
+        m_memory_error();
+        return NULL;
+    }
+
+    w = bmp->width % width;
+    h = bmp->height % height;
+
+    sheet->count = w * h;
+    sheet->sprites = (_Bitmap**) calloc(sheet->count, sizeof(_Bitmap*));
+    if (sheet->sprites == NULL) {
+
+        m_memory_error();
+        return NULL;
+    }
+
+    for (y = 0; y < h; ++ y) {
+   
+        for (x = 0; x < w; ++ x) {
+
+            start = y * h * bmp->width + x * w;
+
+            sheet->sprites[y*w + x] = (_Bitmap*)create_bitmap_from_data(width, height, 
+                    bmp->pixels, bmp->mask,
+                    start, (u32) bmp->width);
+            if (sheet->sprites[y*w + x] == NULL) {
+
+                dispose_sprite_sheet((SpriteSheet*) sheet);
+                return NULL;
+            }
+        }
+    }
+
+    return (SpriteSheet*) sheet;
+}
+
+
+void dispose_sprite_sheet(SpriteSheet* _sheet) {
+
+    _SpriteSheet* sheet = (_SpriteSheet*) _sheet;
+    u16 i;
+    
+    if (sheet == NULL) return;
+
+    for (i = 0; i < sheet->count; ++ i) {
+
+        dispose_bitmap((Bitmap*) sheet->sprites[i]);
+    }
+    m_free(sheet);
+}
--- src/bitmap.h
+++ src/bitmap.h
@@ -6,11 +6,18 @@
 
 
 EMPTY_STRUCT(Bitmap);
+EMPTY_STRUCT(SpriteSheet);
 
 
-Bitmap* new_bitmap(u16 width, u16 height, bool createMash);
+Bitmap* new_bitmap(u16 width, u16 height, bool createMask);
+Bitmap* create_bitmap_from_data(u16 width, u16 height, 
+    u8* pixels, u8* mask, u32 start, u32 offset);
 Bitmap* load_bitmap(str path);
 void dispose_bitmap(Bitmap* bmp);
+
+
+SpriteSheet* create_sprite_sheet_from_bitmap(Bitmap* bmp, u16 width, u16 height);
+void dispose_sprite_sheet(SpriteSheet* sheet);
 
 
 #endif // PROJECTNAME_BITMAP_H
